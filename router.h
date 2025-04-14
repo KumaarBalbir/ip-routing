0021ba4278b505fd7b43ce19afd34bc94d5118bd
balbir <prasadbalbir1056@gmail.com>
2025-04-14 22:34:54 +0530

    router class: store router related info

---
+++ router.h
@@ -0,0 +1,19 @@
+#ifndef ROUTER_H
+#define ROUTER_H
+
+#include <string>
+class Route
+{
+private:
+  std::string routeName;
+  std::string ipAddress;
+  int subnetMask;
+
+public:
+  Route(const std::string &routeName, const std::string &ip, int mask);
+  std::string getRouteName() const;
+  std::string getIPAddress() const;
+  int getSubnetMask() const;
+};
+
+#endif
