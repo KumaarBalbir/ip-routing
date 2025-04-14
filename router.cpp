8a545f4c69b40439f57777dfa9040e3e9731e840
balbir <prasadbalbir1056@gmail.com>
2025-04-14 22:42:35 +0530

    returns router name

--- router.cpp
+++ router.cpp
@@ -6,4 +6,8 @@
 Route::Route(const std::string &routeName, const std::string &ip, int mask)
     : routeName(routeName), ipAddress(ip), subnetMask(mask) {}
 
+std::string Route::getRouteName() const
+{
+  return routeName;
+}
 #endif // ROUTER_CPP
