9279fd4c13d60838e41b7a0f9dc2050d37fdda38
balbir <prasadbalbir1056@gmail.com>
2025-04-15 10:01:09 +0530

    bspl: implement router entryh to hash table

--- bspl/router.cpp
+++ bspl/router.cpp
@@ -2,8 +2,14 @@
 #define ROUTER_CPP
 
 #include "router.h"
+#include "../util.h"
 
 RouterBSPL::RouterBSPL(): prefixTable(IPV4_BIT_LENGTH + 1) {}
 
+void RouterBSPL::insertPrefix(const std::string &ip, int maskLen, const std::string &routeName){
+  std::string prefixIP = ipToBinary(ip).substr(0, maskLen);
+  Entry entry = {prefixIP, true, prefixIP, routeName};
+  prefixTable[maskLen][prefixIP] = entry;
+}
 
 #endif
