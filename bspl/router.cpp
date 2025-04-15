fe48b51f86caa3b36443702bd4afb8d50d0fb37e
balbir <prasadbalbir1056@gmail.com>
2025-04-15 10:17:54 +0530

    bspl: populate hash tble with img entries

--- bspl/router.cpp
+++ bspl/router.cpp
@@ -12,4 +12,26 @@ void RouterBSPL::insertPrefix(const std::string &ip, int maskLen, const std::str
   prefixTable[maskLen][prefixIP] = entry;
 }
 
+void RouterBSPL::insertImgEntries(){
+  std::string lastReal;
+  for(int len = 1; len <= IPV4_BIT_LENGTH; ++len){
+    for(int i = 0; i< (1 << len); ++i)
+    {
+        std::bitset<32> bits(i);
+        std::string prefix = bits.to_string().substr(32-len, len);
+        if(prefixTable[len].find(prefix) == prefixTable[len].end())
+        {
+          // no real IP having 'prefix' as prefix with length len
+          if(!lastReal.empty())
+          {
+             Entry imaginary = {prefix, false, lastReal, ""};
+             prefixTable[len][prefix] = imaginary;
+          }
+        }else if(prefixTable[len][prefix].isReal){
+          lastReal = prefixTable[len][prefix].prefix;
+        }
+    }
+  }
+}
+
 #endif
